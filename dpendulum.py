b89765ad6fdc24f17507997bf7f8d0d74a62392e
Nicolò Cavalieri <nicolo.cavalieri@studenti.unitn.it>
2023-08-08 19:38:23 +0200

    Final version of dpendulum.py
    Minor changes: removed plot_Q_table and added state_size() method to the class dpendulum

--- dpendulum.py
+++ dpendulum.py
@@ -2,6 +2,7 @@ from pendulum import Pendulum
 import numpy as np
 from numpy import pi
 import time
+import matplotlib.pyplot as plt
     
 
 class DPendulum:
@@ -11,16 +12,15 @@ class DPendulum:
         Cost is -1 if the goal state has been reached, zero otherwise.
     '''
     def __init__(self, nbJoint = 1, nu=11, vMax=5, uMax=5, dt=0.2, ndt=1, noise_stddev=0):
-        self.pendulum = Pendulum(nbJoint,vMax,uMax,noise_stddev)
+        self.pendulum = Pendulum(nbJoint, vMax, uMax, noise_stddev)
         self.pendulum.DT  = dt      # discrete-time step
         self.pendulum.NDT = ndt     # integration method step
         self.vMax = vMax            # Max velocity (v in [-vmax,vmax])
-        self.nu = nu                # Number of discretization steps for joint torque
+        self.nu = nu                # Number (must be odd) of discretization steps for joint torque 
         self.uMax = uMax            # Max torque (u in [-umax,umax])
         self.dt = dt                # time step
-        self.DU = 2*uMax/(nu-1)     # modified from nu to (nu - 1) from REPO - this is the discretization resolution for joint torque
+        self.DU = 2*uMax/(nu-1)     # discretization resolution for joint torque
         self.nbJoint = nbJoint      # number of robot joints
-
     
     def state_size(self):
         return self.pendulum.nx
@@ -30,50 +30,44 @@ class DPendulum:
         u = np.clip(u,-self.uMax+1e-3,self.uMax-1e-3)
         return int(np.floor((u+self.uMax)/self.DU))
     
-
     # Discrete to continuous
     def d2cu(self, iu): #joint torques saturation
         iu = np.clip(iu,0,self.nu-1) - (self.nu-1)/2
         return iu*self.DU
     
-    def reset(self,x=None): # come la repo
+    def reset(self,x=None): # continuos time reset
         self.x = self.pendulum.reset(x)
         return self.x
 
-    # COME LA REPO Ok
     def step(self,iu): # simulates the system forward of 1 time step and computes the cost
         self.x, cost = self.dynamics(self.x,iu)
         return self.x, cost
 
-    def render(self): #come la repo
+    def render(self): 
         self.pendulum.render()
         time.sleep(self.pendulum.DT)
 
-    def dynamics(self, x, iu): # ok come la repo (x non usata SI PUO' TOGLIERE)
+    def dynamics(self, x, iu): 
         u   = self.d2cu(iu)
         self.xc,cost = self.pendulum.step(u)
         return self.xc,cost
     
-    def plot_V_table(self, V, q, dq, tabNum = 0): # ok similissimo alla repo
+    def plot_V_table(self, V, q, dq): 
         ''' Plot the given Value table V '''
-        import matplotlib.pyplot as plt
-        
-        plt.figure() # added by Nicolo'
+        plt.figure() 
         plt.pcolormesh(q, dq, V, cmap=plt.cm.get_cmap('Blues'))
         plt.colorbar()
-        plt.title('V table %d ' % tabNum)
+        plt.title("V table")
         plt.xlabel("q")
         plt.ylabel("dq")
         plt.show()
         
-    def plot_policy(self, pi, q, dq, tabNum = 0): # ok similissimo alla repo
+    def plot_policy(self, policy, q, dq): 
         ''' Plot the given policy table pi '''
-        import matplotlib.pyplot as plt
-        
-        plt.figure() # added by Nicolo'
-        plt.pcolormesh(q, dq, pi, cmap=plt.cm.get_cmap('RdBu'))
+        plt.figure()
+        plt.pcolormesh(q, dq, policy, cmap=plt.cm.get_cmap('RdBu'))
         plt.colorbar()
-        plt.title('Policy table %d' % tabNum)
+        plt.title("Policy table")
         plt.xlabel("q")
         plt.ylabel("dq")
         plt.show()
